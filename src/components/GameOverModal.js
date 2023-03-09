e8f987729bc20b1df5df3d5e73553aa51aff0769
Jozef Hudec <jojohudec27@gmail.com>
2023-03-09 05:42:59 +0100

    Improve game over modal

--- src/components/GameOverModal.js
+++ src/components/GameOverModal.js
@@ -1,4 +1,20 @@
+import { useEffect } from 'react'
+
 export default function GameOverModal(props) {
+  useEffect(() => {
+    const blockTab = (e) => {
+      e.preventDefault()
+    }
+
+    document.getElementById('character-cards').addEventListener('keydown', blockTab)
+    document.getElementById('info-btn').addEventListener('keydown', blockTab)
+
+    return () => {
+      document.getElementById('character-cards').removeEventListener('keydown', blockTab)
+      document.getElementById('info-btn').removeEventListener('keydown', blockTab)
+    }
+  }, [])
+
   const { repeatedCard, setRepeatedCard, randomizeCharacters } = props
 
   return (
@@ -6,16 +22,21 @@ export default function GameOverModal(props) {
       <section id="game-over-modal" className="modal">
         <h1>Game Over!</h1>
         <p>You clicked on {repeatedCard} twice.</p>
+        <button
+          style={{ marginTop: 'auto' }}
+          className="play-again-btn"
+          onClick={() => {
+            setRepeatedCard('')
+            randomizeCharacters()
+          }}
+        >
+          Play again
+        </button>
       </section>
 
       <div
         id="game-over-modal-overlay"
         className="overlay"
-        tabIndex="1"
-        onClick={() => {
-          setRepeatedCard('')
-          randomizeCharacters()
-        }}
         onKeyDown={(e) => {
           if (['Enter', 'Space'].includes(e.code)) e.currentTarget.click()
         }}
